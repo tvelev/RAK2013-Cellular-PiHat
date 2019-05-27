45e98386d039c3c4905fb48f801c6eaa2b5f21b1
tvelev <todor.velev@gmail.com>
2019-05-27 16:18:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,19 +13,32 @@ SCRIPT_DIR=$(pwd)
 VERSION="master"
 if [[ $1 != "" ]]; then VERSION=$1; fi
 
-echo "The Things Network Gateway installer"
+echo "RAK Cellular"
 echo "Version $VERSION"
-
-
-# Change hostname if needed
-CURRENT_HOSTNAME=$(hostname)
-
 
 # Check dependencies
 echo "Installing dependencies..."
 apt-get install git libi2c-dev minicom dialog ppp -y
 rak_pppd/ppp-creator.sh HOLOGRAM ttyAMA0
 
+# Install LoRaWAN packet forwarder repositories
+INSTALL_DIR="/opt/RAKLTE"
+if [ ! -d "$INSTALL_DIR" ]; then mkdir $INSTALL_DIR; fi
+pushd $INSTALL_DIR
+
+
+
+cp $SCRIPT_DIR/rak_pppd . -rf
+
+
+
+linenum=`sed -n '/wait_pi_hat_and_ppp/=' /etc/rc.local`
+if [ ! -n "$linenum" ]; then
+	set -a line_array
+	line_index=0
+	for linenum in `sed -n '/exit 0/=' /etc/rc.local`; do line_array[line_index]=$linenum; let line_index=line_index+1; done
+	sed -i "${line_array[${#line_array[*]} - 1]}i/opt/ttn-gateway/rak_pppd/wait_pi_hat_and_ppp.sh" /etc/rc.local
+fi
 
 # add config "dtoverlay=pi3-disable-bt" to config.txt
 linenum=`sed -n '/dtoverlay=pi3-disable-bt/=' /boot/config.txt`
@@ -43,18 +56,10 @@ if [ ! -n "$linenum" ]; then
 	sed -i "${line_array[${#line_array[*]} - 1]}isystemctl stop serial-getty@ttyAMA0.service" /etc/rc.local
 fi
 
-# add rak_script to rc.local
-linenum=`sed -n '/rak_script/=' /etc/rc.local`
-if [ ! -n "$linenum" ]; then
-	set -a line_array
-	line_index=0
-	for linenum in `sed -n '/exit 0/=' /etc/rc.local`; do line_array[line_index]=$linenum; let line_index=line_index+1; done
-	sed -i "${line_array[${#line_array[*]} - 1]}i/usr/local/rak/bin/rak_script" /etc/rc.local
-fi
 
 systemctl disable hciuart
 cd $SCRIPT_DIR
 cp gateway-config /usr/bin/gateway-config
-cp config.txt /boot/config.txt
 
+cp config.txt /boot/config.txt
 
