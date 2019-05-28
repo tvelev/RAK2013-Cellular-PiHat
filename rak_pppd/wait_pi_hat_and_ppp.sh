42b3d37b0a4acf76d55a6c187736fbafa76fc439
tvelev <todor.velev@gmail.com>
2019-05-28 22:17:46 +0300

    Update wait_pi_hat_and_ppp.sh

--- rak_pppd/wait_pi_hat_and_ppp.sh
+++ rak_pppd/wait_pi_hat_and_ppp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd /opt/ttn-gateway/rak_pppd
+cd /opt/RAKLTE/rak_pppd
 ./pi-hat.sh &
-/opt/ttn-gateway/rak_pppd/enable_lte_module.sh
+/opt/RAKLTE/rak_pppd/enable_lte_module.sh
 sleep 5
 ./wait_pi_hat_and_ppp 115200 &
         
